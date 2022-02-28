5826060a030836cbc4f9fecf31024c55b2dbe3ec
Ashik-Ahmed <ashikahmed121@gmail.com>
2022-02-28 21:03:02 +0600

    showing data from api

--- js/index.js
+++ js/index.js
@@ -6,9 +6,26 @@ const loadPhones = () => {
 
 
 const showPhones = (phones) => {
+
+    const searchResultContainer = document.getElementById("search-result");
+
     phones.forEach(phone => {
         const name = phone.phone_name;
         const photo = phone.image;
+
+        const phoneContainer = document.createElement("div");
+        phoneContainer.classList.add("col-12", "col-md-6", "col-lg-4")
+
+        phoneContainer.innerHTML = `
+            <div class="card mb-3" style="width: 18rem;">
+                <img src="${photo}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${name}</h5>
+                    <button class="btn btn-primary">See Details</button>
+                </div>
+            </div>
+        `
+        searchResultContainer.appendChild(phoneContainer);
         console.log(name);
     });
 }
